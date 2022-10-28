0ed505234f0811fd8391ca36dfb808b8f1871ac0
Effa Tatyana <tyanaeffa@yahoo.com>
2022-10-28 15:07:04 +0100

    initial commit

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -5,27 +5,29 @@ import "./Home.css";
 
 export default function Home() {
   return (
-    <div className="home-container">
+    <div>
       <header>
         <Navigation />
       </header>
-      
-      <div className="home1-container">
-        <Link to="/about" className="home-link">
-          About
-        </Link>
 
-        <Link to="/login" className="home-link">
-          Login
-        </Link>
+      <div className="home-container">
+        <div className="home1-container">
+          <Link to="/about" className="home-link">
+            About
+          </Link>
 
-        <Link to="/contact" className="home-link">
-          Contact
-        </Link>
+          <Link to="/login" className="home-link">
+            Login
+          </Link>
 
-        <Link to="/users" className="home-link">
-          Users
-        </Link>
+          <Link to="/contact" className="home-link">
+            Contact
+          </Link>
+
+          <Link to="/users" className="home-link">
+            Users
+          </Link>
+        </div>
       </div>
     </div>
   );
